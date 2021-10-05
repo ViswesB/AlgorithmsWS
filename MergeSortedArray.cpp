c01971534641b02e4a0ce70ca2ab906d4f0483de
ViswesB <visweswar.bhattiprolu@gmail.com>
2021-10-05 11:04:38 +0530

    Day 2 of DS warmup

--- MergeSortedArray.cpp
+++ MergeSortedArray.cpp
@@ -52,7 +52,8 @@ using namespace std;
 
 class Solution {
 public:
-    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+    //using sort not recommended
+    void mergebad(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         //add elements of nums2 to nums1
         int orgSize = nums1.size();
         for(int i=m, j=0;i<orgSize;i++,j++){
@@ -60,6 +61,48 @@ public:
         }
         //sort in increasing/ascending order
         sort(nums1.begin(),nums1.end());
+    }
+
+    // Idea is to use two indexes and point both to end of two vectors
+    // use two pointers from nums1 and nums2 at end
+    // check among two values pointed which one is biggger and move to k (m+n-1) 
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        // size of nums1 final sorted array
+        int k = m+n-1;
+        int i = m-1;
+        int j = n-1;
+    
+        while(k >= 0 && j >= 0 && i >= 0) {
+            //compare value of nums1 to nums2 and find which one is bigger
+            if(nums1[i] > nums2[j]) {
+                //found biggest in nums1 (target itself) so move not copy
+                nums1[k] = nums1[i];
+                nums1[i] = 0; //replace that with 0
+                k--;
+                i--;
+            }
+            else {
+                //found biggest on nums2 vector
+                nums1[k] = nums2[j];
+                k--;
+                j--;
+            }
+        }
+
+        //there could be left over values from either nums1 or nums2 
+        while(k >= 0 && j >=0) {
+            //copy value from nums2[j] to nums1[k]
+            nums1[k] = nums2[j];
+            k--;
+            j--;
+        }
+
+        while(k >= 0 && i >= 0) {
+            //copy values from nums1[i] to nums1[k]
+            nums1[k] = nums1[i];
+            k--;
+            i--;
+        }
     }
 };
 
@@ -69,6 +112,10 @@ int main()
     // vector<int> arrM = {1,2,3,0,0,0};
     // int m = 3;
     // vector<int> arrN = {2,5,6};
+    // int n = 3;
+    // vector<int> arrM = {2,4,8,0,0,0};
+    // int m = 3;
+    // vector<int> arrN = {1,3,5};
     // int n = 3;
     // vector<int> arrM = {1};
     // int m = 1;
